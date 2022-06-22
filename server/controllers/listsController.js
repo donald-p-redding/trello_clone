e156a19a892cfae4fad290e03a96ad34bb3a3fe2
Anne Jones <anne1365@outlook.com>
2022-06-22 08:39:41 -0700

    Debugging list title update functionality Co-authored-by: Don Redding donald.p.redding@gmail.com

--- server/controllers/listsController.js
+++ server/controllers/listsController.js
@@ -3,26 +3,18 @@ const { validationResult } = require("express-validator")
 const List = require('../models/list')
 const Board = require("../models/board")
 
-const createList = (req, resp, next) => {
+const createList = async (req, res, next) => {
   const errors = validationResult(req)
 
   if(errors.isEmpty()) {
     const { boardId, list:{title} } = req.body
-
-    List.create({boardId, title}).then(list => {
-      //add new list to the correct board
-      Board.updateOne(
-        { _id: list.boardId},
-        { $push: { lists: [list._id] } }
-      ).then(response => {
-        console.log(response)
-      })
-
-      //query for a sanatized list to return to front end
-      List.find({_id: list._id}, "title _id boardId createdAt updatedAt position").then(lists => {
-        resp.json(lists[0])
-      })
-    })
+    const  returnList  = await List.create({boardId, title})
+    const  _boardConf  = await Board.updateOne(
+                                { _id: returnList.boardId},
+                                { $push: { lists: [returnList._id] } }
+                               )
+    const  lists  = await List.find({_id: returnList._id}, "title _id boardId createdAt updatedAt position")
+    res.json(lists[0])
   } else {
     return next(new HttpError("Missing 'list' prop in payload or title is empty", 404))
   }
